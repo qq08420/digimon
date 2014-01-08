68d97c742a769c4fe383d9f53b58a8283190469d
qq08420 <605532415@qq.com>
2014-01-08 20:40:26 +0800

    ，
    ，

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
+  response.write("hi,»ù¶ûÊÞÃÇ");
   response.end();
-}).listen(80,function(){console.log("Listen 2333 success")});
+}).listen(80);
