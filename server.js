67abaad4fce893544c5b1c56687088e7420b933a
qq08420 <605532415@qq.com>
2014-01-09 23:02:45 +0800

    .
    .

--- server.js
+++ server.js
@@ -3,17 +3,23 @@ var http    = require('http'),
     io      = require('socket.io');
 http = http.createServer(
     function (req, res) {
-        fs.readFile(__dirname+'/index.html',
+        fs.readFile(__dirname+'/x2.html',
 		function(err, data){res.writeHead(200, {"Content-Type": "text/html"});res.write(data);res.end();});
 	}).listen(80);
 io = io.listen(http);
 io.sockets.on('connection',function(socket){ 
-    socket.emit('msg',{'msg':'connect success'});
-	socket.broadcast.emit('msg',{'msg':'new one connect success'});
+	socket.on('userInfo',function(data){
+	   socket.emit('tip',{'msg':'connect success'});
+	   socket.broadcast.emit('tip',{'msg':data.userName+'success'});
+    });
+    
+	socket.on('tip',function(data){
+        socket.broadcast.emit('tip',data);
+    });
     socket.on('msg',function(data){
         socket.broadcast.emit('msg',data);
     });
-});
-io.sockets.on('disconnection',function(socket){ 
-	socket.broadcast.emit('msg',{'msg':' one leave'});
+	socket.on('disconnect', function(){
+        socket.broadcast.emit('tip',{'msg':socket.id+' leave'});
+    });
 });
